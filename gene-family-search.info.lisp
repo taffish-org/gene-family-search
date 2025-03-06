8f2a737e9fb72716bf207662ca26cf21024b7c17
Kaiyuan Han <hermit_han@163.com>
2025-03-06 17:40:29 +0800

    add dependenciese to info.lisp

--- gene-family-search.info.lisp
+++ gene-family-search.info.lisp
@@ -3,5 +3,8 @@
 (:author         "KaiyuanHan")
 (:version        "1.0.0")
 (:intro          "Gene-Family-Search is a taf flow made by: \"blast, hmmer, cdhit, get-seaqs-from-ids\". This app can use hmm-db's .hhm file and fasta file for blast-makedb, and will use both of these two app to search same gene-family from your input fasta file.")
-(:dependencies)
+(:dependencies   "cdhit-v4.8.1"
+                 "hmmer-v3.4"
+                 "blast-v2.16.0"
+                 "get-seqs-from-ids-v1.0.0")
 (:functions      "[flow]gene-family-search")
