efc6d03aa95501a9cfe5a44a2d3a3df03cc420ab
Kaiyuan Han <hermit_han@163.com>
2025-03-06 10:58:20 +0800

    Initial commit

---
+++ gene-family-search.info.lisp
@@ -0,0 +1,7 @@
+(:name           "gene-family-search")
+(:repo           "HermitHan/gene-family-search")
+(:author         "KaiyuanHan")
+(:version        "1.0.0")
+(:intro          "Gene-Family-Search is a taf flow made by: \"blast, hmmer, cdhit, get-seaqs-from-ids\". This app can use hmm-db's .hhm file and fasta file for blast-makedb, and will use both of these two app to search same gene-family from your input fasta file.")
+(:dependencies)
+(:functions      "[flow]gene-family-search")
